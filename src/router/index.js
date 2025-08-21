d3b275ae2224088b625cc24aef4ed4ed18b69a4f
DevIsola <dev.disola@gmail.com>
2025-08-20 18:05:40 -0700

    Add 404 Not Found page and improve event registration
    Introduces a NotFoundView component and configures a catch-all route to display it for unknown paths. Also updates RegisterView to include a registration button that navigates to the event details page upon clicking.

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import DetailsView from '@/views/event/DetailsView.vue'
 import EditView from '@/views/event/EditView.vue'
 import RegisterView from '@/views/event/RegisterView.vue'
 import LayoutView from '@/views/event/LayoutView.vue'
+import NotFoundView from "@/views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -51,6 +52,11 @@ const router = createRouter({
       name: 'about',
       component: AboutView,
     },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView,
+    }
   ],
 })
 
--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -1,7 +1,18 @@
 <script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
 const props = defineProps(['event'])
+
+const register = () => {
+  router.push({
+    name: 'event-details',
+  })
+}
 </script>
 
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register me!</button>
 </template>
